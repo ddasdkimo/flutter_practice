73548abe44a1f6d1851631006821e39af04865ef
ddasdkimo <david@rai.dscloud.me>
2020-12-04 12:39:02 +0800

    ExpandedTest update

--- lib/page/expanded_test.dart
+++ lib/page/expanded_test.dart
@@ -6,18 +6,41 @@ class ExpandedTest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    // return Row(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       mainAxisSize: MainAxisSize.max,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Expanded(
-          child: Container(color: Colors.red, height: 80),
-          flex: 2,
+          child: Container(
+            color: Colors.red,
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Container(color: Colors.blue, height: 80),
+                  flex: 10,
+                ),
+                Expanded(
+                  child: Container(color: Colors.black, height: 80),
+                  flex: 4,
+                ),
+                Expanded(
+                  child: Container(color: Colors.pink, height: 80),
+                  flex: 4,
+                ),
+              ],
+            ),
+          ),
+          flex: 10,
         ),
         Expanded(
           child: Container(color: Colors.yellow, height: 80),
-          flex: 3,
+          flex: 4,
         ),
         Expanded(
           child: Container(color: Colors.green, height: 80),
